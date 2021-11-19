c9159fd7c9bd324b95034eaeffc742b5d488d52f
Catherine Cabrera <45543137+ccabrera35@users.noreply.github.com>
2021-11-19 18:12:10 -0500

    fix: remove load user on home page

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,17 +1,8 @@
-import { useContext, useEffect } from "react";
-import AuthContext from "../../context/auth/authContext";
 import ContactFilter from "../contacts/ContactFilter";
 import ContactForm from "../contacts/ContactForm";
 import Contacts from "../contacts/Contacts";
 
 const Home = () => {
-    const authContext = useContext(AuthContext);
-
-    useEffect(() => {
-        authContext.loadUser();
-        // eslint-disable-next-line
-    }, []);
-
     return (
         <div className="grid-2">
             <div>
